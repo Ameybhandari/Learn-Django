4f5bacb84aeb3b16fbfe4e9d28441623c18aeadb
Pratik <pratiknarode143@gmail.com>
2023-02-08 17:39:36 +0530

    8th Feb

--- base/models.py
+++ base/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Topic(models.Model):
+    Name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.Name
+    
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    Name = models.CharField(max_length=200)
+    Description = models.TextField(blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.Name
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
+from .models import Room
 # Create your views here.
 
-def home(request):
-    #return HttpResponse('This is Home Page')
-    return render(request, 'home.html') # Using template
+# rooms = [
+#     {'id':1, 'name':'lets learn python'},
+#     {'id':2, 'name':'Design with me '},
+#     {'id':3, 'name':'Front end development'},
+# ]
 
-def room(request):
-    return render(request, 'room.html')
+def home(request):
+    rooms = Room.objects.all()
+    #return HttpResponse('This is the Home Page') using httpresponse
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context) # Using template
+
+def room(request, pk):
+    rooms = Room.objects.get(id=pk)
+    context = {'room':rooms}
+    return render(request, 'base/room.html',context)
